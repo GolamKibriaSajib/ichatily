8154b4bf082710c7fa52a1c79795330278bb8dd5
GolamKibriaSajib <igksajib93@gmail.com>
2018-10-02 02:30:26 +0600

    connect addedddddd

--- register.js
+++ register.js
@@ -3,59 +3,41 @@ module.exports = function (irc, req, res) {
 
   //     var client = new irc.Client('irc.freenode.net', req.body.name);
   // console.log(client);
+   options = {};
+			  options = Object.assign(options, {
+				sasl: false,
+			//	userName: username,
+			//	password: password,
+				debug: true, showErrors: true
+
+			});
+
+			client = new irc.Client("irc.freenode.net", options.userName, options);
   var msg, data;
-  // client.once("registered", function () {
-  //   console.log("jj")
-  //     if (req.body.verify == null) {
-  //         msg = 'register ' + req.body.password + ' ' + req.body.email + ' ';
-  //         console.log(msg)
-  //         client.say('NickServ', msg);
-  //         console.log("redirect");
-  //         res.redirect("./confirm.html");
-  //     }
-  //     if (req.body.verify && req.body.password) {
+  client.once("registered", function () {
+    console.log("jj")
+      if (req.body.verify == null) {
+          msg = 'register ' + req.body.password + ' ' + req.body.email + ' ';
+          console.log(msg)
+          client.say('NickServ', msg);
+          console.log("redirect");
+          res.redirect("./confirm.html");
+      }
+      if (req.body.verify && req.body.password) {
 
-  //         var password = req.body.password;
-  //         client.say('NickServ','identify ' +req.body.name+" "+password);
-  //         data = req.body.verify;
-  //         client.say("NickServ", data)
+          var password = req.body.password;
+          client.say('NickServ','identify ' +req.body.name+" "+password);
+          data = req.body.verify;
+          client.say("NickServ", data)
 
-  //         res.send("you are registered");
-
-
-  //     }
+          res.send("you are registered");
 
 
-  // })
-  var client = new irc.Client('irc.freenode.net', req.body.name, {
-    autoConnect: false,
-    secure: false,
-    sasl:false,
-    debug: true, showErrors: true
-  });
-  client.connect('retryCount', function (serverReply) {
-    console.log("Connected!\n", serverReply);
-    if (req.body.verify == null) {
-      msg = 'register ' + req.body.password + ' ' + req.body.email + ' ';
-      console.log(msg)
-      client.say('NickServ', msg);
-      console.log("redirect");
-      res.redirect("./confirm.html");
-    }
-    if (req.body.verify && req.body.password) {
-
-      var password = req.body.password;
-      client.say('NickServ', 'identify ' + req.body.name + " " + password);
-      data = req.body.verify;
-      client.say("NickServ", data)
-
-      res.send("you are registered");
+      }
 
 
-    }
-
-
-  });
+  })
+  
   client.addListener("error", OnError);
 
   function OnError(message) {
