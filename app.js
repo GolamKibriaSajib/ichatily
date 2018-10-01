aaf96d021d3947babc26b33d28a1e3a8d298175e
GolamKibriaSajib <igksajib93@gmail.com>
2018-10-01 21:40:23 +0600

    k

--- app.js
+++ app.js
@@ -17,6 +17,59 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "/")));
 var port = process.env.PORT || 3000;
+const extendTimeoutMiddleware = (req, res, next) => {
+  const space = ' ';
+  let isFinished = false;
+  let isDataSent = false;
+
+  // Only extend the timeout for API requests
+  if (!req.url.includes('/api')) {
+    next();
+    return;
+  }
+
+  res.once('finish', () => {
+    isFinished = true;
+  });
+
+  res.once('end', () => {
+    isFinished = true;
+  });
+
+  res.once('close', () => {
+    isFinished = true;
+  });
+
+  res.on('data', (data) => {
+    // Look for something other than our blank space to indicate that real
+    // data is now being sent back to the client.
+    if (data !== space) {
+      isDataSent = true;
+    }
+  });
+
+  const waitAndSend = () => {
+    setTimeout(() => {
+      // If the response hasn't finished and hasn't sent any data back....
+      if (!isFinished && !isDataSent) {
+        // Need to write the status code/headers if they haven't been sent yet.
+        if (!res.headersSent) {
+          res.writeHead(202);
+        }
+
+        res.write(space);
+
+        // Wait another 15 seconds
+        waitAndSend();
+      }
+    }, 15000);
+  };
+
+  waitAndSend();
+  next();
+};
+
+// app.use(extendTimeoutMiddleware);
 
 var socketIO = require("./simple3")(io, irc);
 
@@ -51,7 +104,7 @@ app.post("/login", function (req, res) {
 })
 
 
-app.get("/signup",function(req,res){
+app.get("/signup",app.use(extendTimeoutMiddleware),function(req,res){
   res.render("signup.ejs");
 })
 
