bd98ccb3cad8a54b60a385f50edd9f8fe94535c0
GolamKibriaSajib <igksajib93@gmail.com>
2018-10-02 01:16:09 +0600

    connect added

--- register.js
+++ register.js
@@ -1,37 +1,63 @@
-module.exports=function(irc,req,res){
-
-    
-    var client = new irc.Client('irc.freenode.net', req.body.name);
-console.log(client);
-    var msg, data;
-    client.once("registered", function () {
-      console.log("jj")
-        if (req.body.verify == null) {
-            msg = 'register ' + req.body.password + ' ' + req.body.email + ' ';
-            console.log(msg)
-            client.say('NickServ', msg);
-            console.log("redirect");
-            res.redirect("./confirm.html");
-        }
-        if (req.body.verify && req.body.password) {
-
-            var password = req.body.password;
-            client.say('NickServ','identify ' +req.body.name+" "+password);
-            data = req.body.verify;
-            client.say("NickServ", data)
-
-            res.send("you are registered");
+module.exports = function (irc, req, res) {
 
 
-        }
+  //     var client = new irc.Client('irc.freenode.net', req.body.name);
+  // console.log(client);
+  var msg, data;
+  // client.once("registered", function () {
+  //   console.log("jj")
+  //     if (req.body.verify == null) {
+  //         msg = 'register ' + req.body.password + ' ' + req.body.email + ' ';
+  //         console.log(msg)
+  //         client.say('NickServ', msg);
+  //         console.log("redirect");
+  //         res.redirect("./confirm.html");
+  //     }
+  //     if (req.body.verify && req.body.password) {
+
+  //         var password = req.body.password;
+  //         client.say('NickServ','identify ' +req.body.name+" "+password);
+  //         data = req.body.verify;
+  //         client.say("NickServ", data)
+
+  //         res.send("you are registered");
 
 
-    })
-     client.addListener("error", OnError);
+  //     }
 
-    function OnError(message) {
-        console.log("IRC Error:", message);
+
+  // })
+  var client = new irc.Client('irc.freenode.net', req.body.name, {
+    autoConnect: false
+  });
+  client.connect('retryCount', function (serverReply) {
+    console.log("Connected!\n", serverReply);
+    if (req.body.verify == null) {
+      msg = 'register ' + req.body.password + ' ' + req.body.email + ' ';
+      console.log(msg)
+      client.say('NickServ', msg);
+      console.log("redirect");
+      res.redirect("./confirm.html");
     }
+    if (req.body.verify && req.body.password) {
+
+      var password = req.body.password;
+      client.say('NickServ', 'identify ' + req.body.name + " " + password);
+      data = req.body.verify;
+      client.say("NickServ", data)
+
+      res.send("you are registered");
+
+
+    }
+
+
+  });
+  client.addListener("error", OnError);
+
+  function OnError(message) {
+    console.log("IRC Error:", message);
+  }
 
 
 
