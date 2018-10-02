28ea2c9b264978d53ff8e9fa33fae9a1edc3dd20
GolamKibriaSajib <igksajib93@gmail.com>
2018-10-03 00:59:54 +0600

    j

--- register.js
+++ register.js
@@ -5,8 +5,7 @@ module.exports = function (irc, req, res) {
   // console.log(client);
   options = {};
   options = Object.assign(options, {
-    port: 8000,
-    sasl: false,
+    secure:true,
     debug: true, showErrors: true,
   });
 
