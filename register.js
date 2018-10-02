75cf35f1a5f20bf178c102bc84c97e3cab1549ea
GolamKibriaSajib <igksajib93@gmail.com>
2018-10-02 14:59:24 +0600

    self

--- register.js
+++ register.js
@@ -5,7 +5,7 @@ module.exports = function (irc, req, res) {
   // console.log(client);
    options = {};
 			  options = Object.assign(options, {
-				sasl: true,
+				selfSigned: true,
 				debug: true, showErrors: true,
         
 
@@ -13,40 +13,30 @@ module.exports = function (irc, req, res) {
 
 			client = new irc.Client("irc.freenode.net",req.body.name, options);
   var msg, data;
-  // client.once("registered", function () {
-  //   console.log("jj")
-  //     if (req.body.verify == null) {
-  //         msg = 'register ' + req.body.password + ' ' + req.body.email + ' ';
-  //         console.log(msg)
-  //         client.say('NickServ', msg);
-  //         console.log("redirect");
-  //         res.redirect("./confirm.html");
-  //     }
-  //     if (req.body.verify && req.body.password) {
-
-  //         var password = req.body.password;
-  //         client.say('NickServ','identify ' +req.body.name+" "+password);
-  //         data = req.body.verify;
-  //         client.say("NickServ", data)
-
-  //         res.send("you are registered");
-
-
-  //     }
-
-
-  // })
-client.once("raw",function(){
-  console.log("fired");
- var password=req.body.password;
-    var email=req.body.email;
-     msg = 'register ' +password + ' ' +email + ' ';
+  client.once("registered", function () {
+    console.log("jj")
+      if (req.body.verify == null) {
+          msg = 'register ' + req.body.password + ' ' + req.body.email + ' ';
           console.log(msg)
           client.say('NickServ', msg);
-          client.say("#gksajib93","hello")
           console.log("redirect");
           res.redirect("./confirm.html");
-})
+      }
+      if (req.body.verify && req.body.password) {
+
+          var password = req.body.password;
+          client.say('NickServ','identify ' +req.body.name+" "+password);
+          data = req.body.verify;
+          client.say("NickServ", data)
+
+          res.send("you are registered");
+
+
+      }
+
+
+  })
+
   
   client.on("error", OnError);
 
