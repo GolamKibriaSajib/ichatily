db56405dbc78eafd587201158a20bd45d5c428f4
GolamKibriaSajib <igksajib93@gmail.com>
2018-10-03 11:48:52 +0600

    jjkklfee

--- register.js
+++ register.js
@@ -5,7 +5,7 @@ module.exports = function (irc, req, res) {
   // console.log(client);
   options = {};
   options = Object.assign(options, {
-    selfSigned: true,
+    channels:["#gksajib93"],
     debug: true, showErrors: true,
   });
 
