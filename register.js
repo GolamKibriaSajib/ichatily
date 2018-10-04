c86541a1b0b83159b8f0916436f1f03ccc77c12c
GolamKibriaSajib <igksajib93@gmail.com>
2018-10-04 17:58:25 +0600

    jkioop

--- register.js
+++ register.js
@@ -6,12 +6,9 @@ module.exports = function (irc, req, res) {
   options = {};
   options = Object.assign(options, {
     debug: true, showErrors: true, 
-    autoConnect:false
   });
 
   var client = new irc.Client("irc.freenode.net", req.body.name, options);
-  console.log(client);
-  client.join("#gksajib93")
   var msg, data;
   client.once("registered", function () {
     if (req.body.verify == null) { 
